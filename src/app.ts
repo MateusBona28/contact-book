3e444c9c1a4b77516d9453cdec04210c6e7f5a0e
mateus <mateusbona28@gmail.com>
2022-12-07 16:16:57 -0300

    feat: adicionando middleware para tratamento de erros globais, e também rotas para criação de conta na aplicação.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
+import 'express-async-errors';
 import express from 'express';
 import accountsRoutes from './views/accounts.view';
+import { handleErrorMiddleware } from './middlewares/handleError.middleware';
 
 const app = express();
 app.use(express.json());
 
-app.use('/accounts', accountsRoutes)
+app.use('/accounts', accountsRoutes);
+
+app.use(handleErrorMiddleware);
 
 export default app;
